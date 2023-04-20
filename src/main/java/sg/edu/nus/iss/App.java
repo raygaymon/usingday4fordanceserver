acd76301bb25f231eadd0190b8a30c084489ff8d
raygaymon <raysonlim.jr@gmail.com>
2023-04-20 15:28:06 +0800

    server

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,13 +1,89 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
-    }
+        //2 arguments
+        //1 for file 1 for starting server port 
+        String fileName = args[0];
+        String port = args[1];
+
+        File fortuneCookie = new File(fileName);
+        if (!fortuneCookie.exists()) {
+            System.out.println("Cookies not found. Please go exercise instead.");
+            System.exit(0);
+        } else {
+            System.out.println("Fortune Cookie found. Preparing fortunes for dispensing.");
+        }
+
+        Cookie cooks = new Cookie();
+        cooks.cookieRead(fileName); // use the fileName for this function
+        String fortune = cooks.getRandomCookie();
+        System.out.println(fortune);
+
+        ServerSocket server = new ServerSocket(Integer.parseInt(port));
+        Socket socket = server.accept();
+
+        //allows server to read and write over comms channel
+        try (InputStream is = socket.getInputStream()) {
+            BufferedInputStream bis = new BufferedInputStream (is);
+            DataInputStream disNuts = new DataInputStream (bis);
+
+            //stores data sent over from client like commands and stuff
+            //this particular command can be outside the server code nest also
+            String clientMSG = "";
+
+            try(OutputStream os = socket.getOutputStream()) {
+                BufferedOutputStream bos = new BufferedOutputStream (os);
+                DataOutputStream dos = new DataOutputStream (bos);
+                
+
+                //code here will deal with sending and receiving
+                while (!clientMSG.equals("close")) {
+                    //receiving message
+                    clientMSG = disNuts.readUTF();
+                    if (clientMSG.equals("get-cookie")) {
+                        //instantiate cookie.java 
+                        //deliver the inspo
+                        //send the cookie to client side with data output
+                    }   dos.writeUTF("What you want");
+                        dos.flush();
+                }
+
+                //closes all output streams in reverse order
+                dos.close();
+                bos.close();
+                os.close();
+
+            } catch (EOFException ex) {
+                ex.printStackTrace();
+            }
+            //closes all input streams in reverse order
+            disNuts.close();
+            bis.close();
+            is.close();
+
+        } catch (EOFException ex) {
+            socket.close();
+        }
+
+
+    }  
 }
---
+++ src/main/java/sg/edu/nus/iss/Cookie.java
@@ -0,0 +1,64 @@
+package sg.edu.nus.iss;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.lang.Math;
+
+public class Cookie {
+
+    List<String> cookieInspos = null;
+
+    // function input is the cookie file with all the cookies | FileNotFoundException because we dealing with files...?
+    public void cookieRead(String fileName) throws IOException, FileNotFoundException {
+
+        // instantiate cookie collection
+        cookieInspos = new ArrayList<>(); //correct
+
+        // get a file object to pass the fileName
+        File cookieSource = new File(fileName); //correct
+
+        // use FileReader and BufferedReader for reading the cookie file
+        FileReader fr = new FileReader(cookieSource); //originlly was fileName, read the file that was just instantiated in step above
+        BufferedReader br = new BufferedReader(fr); //correct
+
+        // while loop to loop through the file
+        String reading = ""; //correct
+        int i = 0;
+        while ((reading = br.readLine()) != null) {
+
+            // read each cookie line and add into collection object
+            System.out.println(i + "cookie(s) loaded");
+            cookieInspos.add(reading); //correct
+            i++;
+            //no need to have extra br.readline it's already running in for-loop
+        }
+
+        System.out.println("Cookies loaded and ready to crumble");
+
+        // close the readers
+        br.close();
+        fr.close();
+    }
+
+    public String getRandomCookie() {
+
+        //math random function to get a random item from cookie object
+        
+        Random random = new Random(); // random must be defined first before using
+
+        //must make sure cookies are loaded in cookie colection first
+        if (cookieInspos == null) {
+            System.out.println("Your cookie tray is empty, you have no luck today");
+        } 
+
+        //if cookies are loaded, then send a random one
+        String drawnCookie = cookieInspos.get(random.nextInt(cookieInspos.size())); // use random with .nextInt to get number
+        return drawnCookie;
+    }
+}
